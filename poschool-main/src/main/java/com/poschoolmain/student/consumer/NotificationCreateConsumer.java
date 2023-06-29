b2ea0aacc99c63696a27b9e1a16821472f36456c
oddnine <dnwnsgur12@gmail.com>
2023-06-30 08:56:15 +0900

    노티피케이션 로깅

--- poschool-main/src/main/java/com/poschoolmain/student/consumer/NotificationCreateConsumer.java
+++ poschool-main/src/main/java/com/poschoolmain/student/consumer/NotificationCreateConsumer.java
@@ -1,22 +1,39 @@
 package com.poschoolmain.student.consumer;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.poschoolmain.student.producer.EnrollmentCreatedProducer;
+import com.poschoolmain.student.service.StudentService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
-public class EnrollmentStateConsumer {
-//    private final OrderService orderService;
+public class NotificationCreateConsumer {
     private final ObjectMapper objectMapper;
+    private final StudentService studentService;
 
-//    @KafkaListener(topics = "enrollment_state", groupId = "student")
-//    public void consume(String jsonObject) {
-//        try {
-//            var order = objectMapper.readValue(jsonObject, OrderState.class);
-//            orderService.state(order);
-//        } catch (Exception e) {
-//
-//        }
-//    }
+    private static final Logger logger = LoggerFactory.getLogger(NotificationCreateConsumer.class);
+
+    @KafkaListener(topics = "notification_create", groupId = "student")
+    public void consume(String jsonObject) {
+        try {
+            // JSON 파싱
+            JSONObject parsing = new JSONObject(jsonObject);
+
+            // "message" 필드의 값 가져오기
+            String message = parsing.getString("message");
+
+            logger.info("consumer: notification_create: " + message);
+
+            studentService.requestResult(message);
+        } catch (Exception e) {
+
+        }
+    }
 }
