f078360c57f078c5b427e01a127f536f6a6d83e7
nanajin <nmj5182@gmail.com>
2023-06-29 13:47:07 +0900

    feat: history controller, consumer 수정

--- poschool-enrollmenthistory/src/main/java/com/poschoolenrollmenthistory/controller/EnrollmentHistoryController.java
+++ poschool-enrollmenthistory/src/main/java/com/poschoolenrollmenthistory/controller/EnrollmentHistoryController.java
@@ -16,17 +16,13 @@ public class EnrollmentHistoryController {
     private final EnrollmentHistoryService enrollmentHistoryService;
     @PostMapping("/savehistory")
     public ResponseEntity<String> saveHistory(@RequestBody HashMap<String, Object> map) {
-//        Long saveId = enrollmentHistoryService.save(EnrollmentHistory.builder()
-//                        .course_id(map.get("courseId").toString())
-//                        .student_id(map.get("studentId").toString())
-//                        .semester(map.get("semester").toString())
-//                        .enrollment_history_status(map.get("enrollmentHistoryStatus").toString())
-//                        .build());
-//                .courseName(map.get("courseName").toString())
-//                .maxCount(Integer.parseInt(map.get("maxCount").toString()))
-//                .currentCount(0)
-//                .build());
+        Long saveId = enrollmentHistoryService.save(EnrollmentHistory.builder()
+                        .course_id(Long.valueOf(map.get("courseId").toString()))
+                        .student_id(Long.valueOf(map.get("studentId").toString()))
+                        .semester(map.get("semester").toString())
+                        .enrollment_history_status(map.get("enrollmentHistoryStatus").toString())
+                        .build());
 
-        return ResponseEntity.ok("로그가 저장되었습니다.");
+        return ResponseEntity.ok("신청 내역에 대한 로그가 저장되었습니다.");
     }
 }
